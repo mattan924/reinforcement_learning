807803e09a151bdc54ab7250aea72f9f0f4a2691
rmatsumoto <rmatsumoto@fine.cs.kobe-u.ac.jp>
2023-03-23 17:35:21 +0900

    train_loopの関数化　一時保存

--- src/training_loop.py
+++ src/training_loop.py
@@ -1,5 +1,5 @@
 from env import Env
-from COMA import COMA, ActorCritic
+from COMA import COMA
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
@@ -21,61 +21,47 @@ def read_train_curve(log_path, pre_train_iter):
     
     return reward_history
 
-if __name__ == '__main__':
+def train_loop(max_epi_itr, device, result_dir, learning_data_index_path, log_file, err_file, start_epi_itr=0, load_parameter_path=None):
     # 各種パラメーター
-    max_epi_itr = 1000
     N_action = 9
     buffer_size = 3000
     batch_size = 500
-    device = 'cuda:0'
+    backup_iter = 1000
 
-    #  train_flag = True: 学習モード, False: 実行モード
-    train_flag = True
     #  pretrain_flag = True: 指定の行動, False: 確率分布からサンプリング
     pretrain_flag = False
-    #  load_flag = True: 重みパラメータを読み込む, False: 読み込まない
-    load_flag = False
-    #  学習開始時のエピソード数を指定
-    start_epi_itr = 0
     #  pre_trainを行うエピソードの周期 (pre_train_iter = 10の時10回に一回 pre_train を実行)
     pre_train_iter = 10
-    #  重みのバックアップを行うエピソードの周期 (backup_iter = 1000 の時1000回に一回バックアップを実行)
-    backup_iter = 1000
+
+    #  load_flag = True: 重みパラメータを読み込む, False: 読み込まない
+    if start_epi_itr != 0:
+        if load_parameter_path == None:
+            sys.exit("読み込む重みパラメータのパスを指定してください")
+        else:
+            load_flag = True
+    else:
+        load_flag = False
+
     #  価値関数とActor ネットワークを交互に固定して学習するためのフラグ
     fix_net_flag = False
     #  何エピソードごとに固定する方を変えるか
     fix_net_iter = 10
 
-    #  重みパラメータ、学習結果を保存するディレクトリの確認
-    specified_dir_name = "COMA_fix_test/no_fix"
-    result_dir = "../result/" + specified_dir_name + "/"
-
     if not os.path.isdir(result_dir + "model_parameter"):
         sys.exit("結果を格納するディレクトリ" + result_dir + "model_parameter が作成されていません。")
 
     #  標準エラー出力先の変更
-    sys.stderr = open(result_dir + "err.log", 'w')
-
-    #  各種ログの出力先ファイルの指定
-    log_file = result_dir + "out.log"
-    pi_dist_file = result_dir + "pi_dist.log"
+    sys.stderr = open(err_file, 'w')
 
     if load_flag == False:
         with open(log_file, 'w') as f:
             pass
 
-        with open(pi_dist_file, "w") as f:
-            pass
-
-    #  学習に使用するデータの指定
-    learning_data_index = "../dataset/learning_data/index/index_test.csv"
-
     #  環境のインスタンスの生成
-    env = Env(learning_data_index)
+    env = Env(learning_data_index_path)
 
     #  学習モデルの指定
     agent = COMA(N_action, env.num_client, buffer_size, batch_size, device)
-    #agent = ActorCritic(N_action, env.num_client, buffer_size, batch_size, device)
 
     #  学習による total_reward の推移を保存
     train_curve = []
@@ -110,7 +96,7 @@ if __name__ == '__main__':
                 pretrain_flag = False
 
             #  行動と確率分布の取得
-            actions, pi = agent.get_acction(obs, env, train_flag, pretrain_flag)
+            actions, pi = agent.get_acction(obs, env, True, pretrain_flag)
 
             # 報酬の受け取り
             reward = env.step(actions, time)
@@ -132,17 +118,13 @@ if __name__ == '__main__':
             with open(log_file, 'a') as f:
                 f.write(f"{(epi_iter/max_epi_itr)*100}%, {-sum(reward_history)}\n")
 
-            with open(pi_dist_file, "a") as f:
-                for i in range(1):
-                    f.write(f"agent {i} pi = {pi[i]}\n")
-
         #  重みパラメータのバックアップ
         if epi_iter % backup_iter == 0:
             agent.save_model(result_dir + 'model_parameter/', epi_iter)
             load_flag = True
 
     #  最適解の取り出し
-    df_index = pd.read_csv(learning_data_index, index_col=0)
+    df_index = pd.read_csv(learning_data_index_path, index_col=0)
     opt = df_index.at['data', 'opt']
 
     train_curve = read_train_curve(log_file, pre_train_iter)
