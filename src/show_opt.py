fdffa571fbeaae9cd93bb11f399731d1cf55aca9
rmatsumoto <R.Matsumoto924@gmail.com>
2023-05-02 11:16:28 +0900

    追加

--- src/show_opt.py
+++ src/show_opt.py
@@ -4,10 +4,10 @@ import util
 import animation
 
 
-index_file = "../dataset/learning_data/index/index_multi.csv"
+index_file = "../dataset/learning_data/index/index_multi2.csv"
 
-opt_solution_file = "../dataset/learning_data/solution/solution_multi.csv"
+opt_solution_file = "../dataset/learning_data/solution/solution_multi2.csv"
 
-output_file = "../dataset/execution_data/animation/multi_opt.gif"
+output_file = "../dataset/execution_data/animation/multi_opt2.gif"
 
 animation.create_opt_animation(index_file, output_file, opt_solution_file, 20)
