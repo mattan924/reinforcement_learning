5600fcd36b6f9c9f200597799a2635fa17f8ab5f
rmatsumoto <R.Matsumoto924@gmail.com>
2023-10-10 19:16:36 +0900

    最適解の追加

--- src/solve_opt.py
+++ src/solve_opt.py
@@ -3,8 +3,8 @@ import pandas as pd
 import time as time_modu
 
 if __name__ == '__main__':
-    index_file = "../dataset/data_set_hard/test/index/index_hard_9.csv"
-    output_file = "../dataset/data_set_hard/test/solution/solution_hard_9_opt.csv"
+    index_file = "../dataset/dataset_hard/test/index/index_hard_9.csv"
+    output_file = "../dataset/dataset_hard/test/solution/solution_hard_9_opt.csv"
 
     start_time = 0
 
